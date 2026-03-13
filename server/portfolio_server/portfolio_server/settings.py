cb8bbf0c8fca04395a64fc342425b0a9323f5eb0
Kailash Sri <kailash.sri@outlook.com>
2026-03-12 23:38:10 -0400

    trigger redeploy

--- server/portfolio_server/portfolio_server/settings.py
+++ server/portfolio_server/portfolio_server/settings.py
@@ -143,4 +143,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
